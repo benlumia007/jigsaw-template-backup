525731c58d2ea5280467423833163028b4b46740
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-01-16 00:58:59 -0800

    initial commit

---
+++ app/framework.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Framework instance
+ * 
+ * This file is used to create a new framework instance and adds specific features to the theme.
+ * 
+ * @package   Benlu
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Create a new framework instance.
+ */
+$benlu = new Benlumia007\Alembic\Framework();
+
+/**
+ * Loads the correct domain name.
+ */
+$config = require_once( 'app/functions-config.php' );
+
+/**
+ * Register default instances.
+ */
+$benlu->instance( 'path', ROOT_DIR );
+$benlu->instance( 'uri', $config['uri'] );
+$benlu->instance( 'uri/relative', parse_url( $benlu->uri, PHP_URL_PATH ) );
+$benlu->instance( 'path/content', 'user/content' );
+$benlu->instance( 'cache/meta', [ 'date', 'category', 'slug' ] );
+$benlu->boot();
+
+/**
+ * Add default content types.
+ */
+Benlumia007\Alembic\ContentTypes::add( 'page', new Benlumia007\Alembic\Entry\Types\Page( $benlu->routes ) );
+
+/**
+ * Register content types
+ */
+Benlumia007\Alembic\ContentTypes::registerRoutes();
+
+/**
+ * Register home content type
+ */
+$benlu->routes->get( '/', Benlumia007\Alembic\Entry\Controllers\Home::class );
+
+// Launch the current controller.
+$current = $benlu->routes->current();
---
+++ app/functions-config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Config default uri
+ * 
+ * This file is used to return the uri that's getting use. This can be
+ * change depending on if you are using locally or production.
+ * 
+ * @package   Succotash
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Loads the correct domain name uri.
+ */
+return [
+	'uri' => 'https://benlu.test',
+];
