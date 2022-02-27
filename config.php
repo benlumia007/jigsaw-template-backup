41919d99ce8f91602ca8c01f29790955d84d1398
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-02-26 20:33:43 -0800

    make sure that selected is setup for menu

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'siteName'        => 'Benjamin Lu',
-    'siteDescription' => 'Life & Joy'
+    'siteDescription' => 'Life & Joy',
+
+    'selected' => function( $page, $selection ) {
+        return Str::contains( $page->getPath(), $selection ) ? 'active' : '';
+    },
 ];
