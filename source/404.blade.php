d48df98166e83349105442105693010cf6bc5b66
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-02-28 14:17:41 -0800

    add 404

---
+++ source/404.blade.php
@@ -0,0 +1,9 @@
+@extends( '_layouts.main' )
+
+@section( 'body' )
+    <section id="content" class="site-content">
+        <div class="content-area">
+            not found
+        </div>
+    </section>
+@endsection
