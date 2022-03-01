017511bf25db6ef5fd88c416106b2380f710c32a
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-01 00:04:47 -0800

    udpated

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,12 @@ mix.setPublicPath( 'source/assets/scripts' );
  * @link https://laravel.com/docs/5.6/mix#working-with-scripts
  */
 mix.jigsaw()
-      .js( `source/_assets/js/app.js`, 'js' )
+    .js( `source/_assets/js/app.js`, 'js' )
     .sass( `source/_assets/scss/screen.scss`, 'css' )
     .options( {
         processCssUrls: false,
     } )
+    .browserSync({
+        server: 'build_local',
+        files: ['build_*/**'],
+    });
